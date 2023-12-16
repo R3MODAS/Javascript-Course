03f3966dec8df1e6ef1ded7a6c2da720817fce07
Sharadindu Das <remodas7774@gmail.com>
2023-12-16 21:57:17 +0530

    async js

---
+++ 09_async/01_async.js
@@ -0,0 +1,21 @@
+/*
+    -> Javascript (Default)
+        - Synchronous
+        - Single threaded
+
+    JS Engine (V8 engine written in C++) is used as an runtime environment either by browser or nodejs
+
+    -> Execution Context
+        - execute one line of code at a time
+
+        - console log -> 1           // Each operation waits for the last one
+        - console log -> 2              to complete before executing            
+
+        Call Stack    Memory Heap
+
+
+
+
+
+
+*/
